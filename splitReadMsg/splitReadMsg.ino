5474f122f98c11e291dee59341b9fc81769937c6
Z4K64 <90631798+Z4K64@users.noreply.github.com>
2025-04-22 18:48:55 +0100

    allVerses functionality added

--- splitReadMsg/splitReadMsg.ino
+++ splitReadMsg/splitReadMsg.ino
@@ -25,6 +25,32 @@ SplitResult splitMessage(String message, char delimiter);
 
 int numSurah = 0;
 
+// Lookup table: key 1 => 100, key 2 => 200, ..., key 5 => 500
+const int valueMap[] = {
+  7, 286, 200, 176, 120, 165, 206, 75, 129, 109,
+  123, 111, 43, 52, 99, 128, 111, 110, 98, 135,
+  112, 78, 118, 64, 77, 227, 93, 88, 69, 60,
+  34, 30, 73, 54, 45, 83, 182, 88, 75, 85,
+  54, 53, 89, 59, 37, 35, 38, 29, 18, 45,
+  60, 49, 62, 55, 78, 96, 29, 22, 24, 13,
+  14, 11, 11, 18, 12, 12, 30, 52, 52, 44,
+  28, 28, 20, 56, 40, 31, 50, 40, 46, 42,
+  29, 19, 36, 25, 22, 17, 19, 26, 30, 20,
+  15, 21, 11, 8, 8, 19, 5, 8, 8, 11,
+  11, 8, 3, 9, 5, 4, 7, 3, 6, 3,
+  5, 4, 5, 6
+};
+
+const int NUM_KEYS = sizeof(valueMap) / sizeof(valueMap[0]);
+
+// Function to safely look up value based on 1-based key
+int lookup(int key) {
+  if (key >= 1 && key <= NUM_KEYS) {
+    return valueMap[key - 1];
+  }
+  return -1;  // Key not found
+}
+
 void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -124,6 +150,11 @@ void setup() {
           //SURAH WITH ALL VERSES ADDED
           Serial.println("all verses");
           chosenSurahs[j] = combos.parts[j].toInt(); //Need to add sting to int
+          int numOfVerse = lookup(chosenSurahs[j]);  
+             
+          for (int o = 0; o < numOfVerse; o++){
+            chosenVerses[j][o] = o + 1;
+          }  
         }
       
       }
